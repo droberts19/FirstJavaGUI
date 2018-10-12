7f242c220ed8183b6601a16e348b68b46b1e41f1
droberts19 <droberts19@scienceleadership.org>
2018-10-12 13:54:27 -0400

    yes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,60 +6,73 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.RadioButton;
-import java.awt.Graphics;
-import javax.swing.ImageIcon;
-import java.awt.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
 
-    private boolean phillyPic;
-    private boolean chicagoPic;
-    private boolean laPic;
-    private boolean nycPic;
-    private boolean seattlePic;
+    private boolean phillyPic2 = true;
+    private boolean chicagoPic2 = false;
+    private boolean laPic2 = false;
+    private boolean nycPic2 = false;
+    private boolean seattlePic2 = false;
 
-    public void displayImages(Graphics g) {
+    private Image phillyImage;
+    private Image chicagoImage;
+    private Image laImage;
+    private Image nycImage;
+    private Image seattleImage;
 
-        ImageIcon a = new ImageIcon("philly.png");
-        Image Philly = a.getImage();
-        int x = 0;
-        int y = 50;
-        if (phillyPic = true) {
-            g.drawImage(Philly, x, y, 500, 240, null);
+    private ImageView myImageView;
+
+    public void displayImages() {
+
+        if (phillyPic2 = true) {
+            myImageView.setImage(phillyImage);
         }
 
-        ImageIcon b = new ImageIcon("chicago.jpg");
-        Image Chicago = b.getImage();
-        if (chicagoPic = true) {
-            g.drawImage(Chicago, x, y, 500, 333, null);
+        if (chicagoPic2 = true) {
+            myImageView.setImage(chicagoImage);
         }
 
-        ImageIcon c = new ImageIcon("LA.jpg");
-        Image LA = c.getImage();
-        //g.drawImage(LA, x, y, 500, 225, null);
+        if (laPic2 = true) {
+            myImageView.setImage(laImage);
+        }
 
-        ImageIcon d = new ImageIcon("NYC.jpeg");
-        Image NYC = d.getImage();
-        //g.drawImage(NYC, x, y, 500, 263, null);
+        if (nycPic2 = true) {
+            myImageView.setImage(nycImage);
+        }
 
-        ImageIcon e = new ImageIcon("seattle.jpg");
-        Image Seattle = e.getImage();
-        //g.drawImage(Seattle, x, y, 500, 333, null);
+        if (seattlePic2 = true) {
+            myImageView.setImage(seattleImage);
+        }
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Image Slide Show");
 
-        displayImages();
+        phillyImage = new Image("philly.png");
+        myImageView = new ImageView(phillyImage);
+        chicagoImage = new Image("chicago.jpg");
+        laImage = new Image("LA.jpg");
+        nycImage = new Image("NYC.jpeg");
+        seattleImage = new Image("seattle.jpg");
+
 
         ToggleGroup group = new ToggleGroup();
         RadioButton phillyPic = new RadioButton("Philly");
         phillyPic.setToggleGroup(group);
-        phillyPic.setSelected(false);
+        phillyPic.setSelected(true);
         phillyPic.setOnAction(e->{
                     System.out.println("phillyPic action happened");
+                    phillyPic2 = true;
+                    chicagoPic2 = false;
+                    laPic2 = false;
+                    nycPic2 = false;
+                    seattlePic2 = false;
+                    displayImages();
                 }
         );
         RadioButton chicagoPic = new RadioButton("Chicago");
@@ -67,6 +80,12 @@ public class Main extends Application {
         chicagoPic.setSelected(false);
         chicagoPic.setOnAction(e->{
                     System.out.println("chicagoPic action happened");
+                    phillyPic2 = false;
+                    chicagoPic2 = true;
+                    laPic2 = false;
+                    nycPic2 = false;
+                    seattlePic2 = false;
+                    displayImages();
                 }
         );
         RadioButton laPic = new RadioButton("LA");
@@ -74,6 +93,12 @@ public class Main extends Application {
         laPic.setSelected(false);
         laPic.setOnAction(e->{
                     System.out.println("laPic action happened");
+                    phillyPic2 = false;
+                    chicagoPic2 = false;
+                    laPic2 = true;
+                    nycPic2 = false;
+                    seattlePic2 = false;
+                    displayImages();
                 }
         );
         RadioButton nycPic = new RadioButton("NYC");
@@ -81,6 +106,12 @@ public class Main extends Application {
         nycPic.setSelected(false);
         nycPic.setOnAction(e->{
                     System.out.println("nycPIC action happened");
+                    phillyPic2 = false;
+                    chicagoPic2 = false;
+                    laPic2 = false;
+                    nycPic2 = true;
+                    seattlePic2 = false;
+                    displayImages();
                 }
         );
         RadioButton seattlePic = new RadioButton("Seattle");
@@ -88,9 +119,14 @@ public class Main extends Application {
         seattlePic.setSelected(false);
         seattlePic.setOnAction(e->{
                     System.out.println("seattlePic action happened");
+                    phillyPic2 = false;
+                    chicagoPic2 = false;
+                    laPic2 = false;
+                    nycPic2 = false;
+                    seattlePic2 = true;
+                    displayImages();
                 }
         );
-
 
         VBox layout = new VBox();
         layout.getChildren().add(phillyPic);
@@ -98,11 +134,11 @@ public class Main extends Application {
         layout.getChildren().add(laPic);
         layout.getChildren().add(nycPic);
         layout.getChildren().add(seattlePic);
+        layout.getChildren().add(myImageView);
 
         primaryStage.setScene(new Scene(layout, 500, 500));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
