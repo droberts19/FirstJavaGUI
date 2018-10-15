e230087fd4a8977eeae3706e5c1b6f8fedec4509
droberts19 <droberts19@scienceleadership.org>
2018-10-15 14:24:27 -0400

    yes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -25,41 +25,21 @@ public class Main extends Application {
 
     private ImageView myImageView;
 
-    public void displayImages() {
-
-        if (phillyPic2 = true) {
-            myImageView.setImage(phillyImage);
-        }
-
-        if (chicagoPic2 = true) {
-            myImageView.setImage(chicagoImage);
-        }
-
-        if (laPic2 = true) {
-            myImageView.setImage(laImage);
-        }
-
-        if (nycPic2 = true) {
-            myImageView.setImage(nycImage);
-        }
-
-        if (seattlePic2 = true) {
-            myImageView.setImage(seattleImage);
-        }
-    }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Image Slide Show");
 
         phillyImage = new Image("philly.png");
-        myImageView = new ImageView(phillyImage);
         chicagoImage = new Image("chicago.jpg");
         laImage = new Image("LA.jpg");
         nycImage = new Image("NYC.jpeg");
         seattleImage = new Image("seattle.jpg");
+        myImageView = new ImageView(phillyImage);
 
+        myImageView.setFitHeight(900);
+        myImageView.setFitWidth(1000);
+        myImageView.setPreserveRatio(true);
 
         ToggleGroup group = new ToggleGroup();
         RadioButton phillyPic = new RadioButton("Philly");
@@ -67,12 +47,7 @@ public class Main extends Application {
         phillyPic.setSelected(true);
         phillyPic.setOnAction(e->{
                     System.out.println("phillyPic action happened");
-                    phillyPic2 = true;
-                    chicagoPic2 = false;
-                    laPic2 = false;
-                    nycPic2 = false;
-                    seattlePic2 = false;
-                    displayImages();
+                    myImageView.setImage(phillyImage);
                 }
         );
         RadioButton chicagoPic = new RadioButton("Chicago");
@@ -80,12 +55,7 @@ public class Main extends Application {
         chicagoPic.setSelected(false);
         chicagoPic.setOnAction(e->{
                     System.out.println("chicagoPic action happened");
-                    phillyPic2 = false;
-                    chicagoPic2 = true;
-                    laPic2 = false;
-                    nycPic2 = false;
-                    seattlePic2 = false;
-                    displayImages();
+                    myImageView.setImage(chicagoImage);
                 }
         );
         RadioButton laPic = new RadioButton("LA");
@@ -93,12 +63,7 @@ public class Main extends Application {
         laPic.setSelected(false);
         laPic.setOnAction(e->{
                     System.out.println("laPic action happened");
-                    phillyPic2 = false;
-                    chicagoPic2 = false;
-                    laPic2 = true;
-                    nycPic2 = false;
-                    seattlePic2 = false;
-                    displayImages();
+                    myImageView.setImage(laImage);
                 }
         );
         RadioButton nycPic = new RadioButton("NYC");
@@ -106,12 +71,7 @@ public class Main extends Application {
         nycPic.setSelected(false);
         nycPic.setOnAction(e->{
                     System.out.println("nycPIC action happened");
-                    phillyPic2 = false;
-                    chicagoPic2 = false;
-                    laPic2 = false;
-                    nycPic2 = true;
-                    seattlePic2 = false;
-                    displayImages();
+                    myImageView.setImage(nycImage);
                 }
         );
         RadioButton seattlePic = new RadioButton("Seattle");
@@ -119,12 +79,7 @@ public class Main extends Application {
         seattlePic.setSelected(false);
         seattlePic.setOnAction(e->{
                     System.out.println("seattlePic action happened");
-                    phillyPic2 = false;
-                    chicagoPic2 = false;
-                    laPic2 = false;
-                    nycPic2 = false;
-                    seattlePic2 = true;
-                    displayImages();
+                    myImageView.setImage(seattleImage);
                 }
         );
 
@@ -136,7 +91,7 @@ public class Main extends Application {
         layout.getChildren().add(seattlePic);
         layout.getChildren().add(myImageView);
 
-        primaryStage.setScene(new Scene(layout, 500, 500));
+        primaryStage.setScene(new Scene(layout, 1000, 1000));
         primaryStage.show();
     }
 
